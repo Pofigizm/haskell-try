bbf818bac0302a8da5dc5548cdada2f247a2fd3b
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 02:17:53 +0300

    other solution if-then-else function

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 -- import Data.Text
 -- import Helpers
 
-main = putStrLn (indicate "127.0.0.2")
+main = putStrLn (indicate "")
 
 indicate :: String -> String
-indicate "127.0.0.1" = "localhost"
-indicate address = address
+indicate address
+  | address == "127.0.0.1" = "localhost"
+  | null address = "empty Ip-address"
+  | otherwise = address
