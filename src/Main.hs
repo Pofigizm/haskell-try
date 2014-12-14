edacf9b9299ba7b17c4149791fbf3e5fca94d750
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 03:15:00 +0300

    carrying function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
 -- import Data.Text
 -- import Helpers
 
-main = print ((\ x -> x * x) 5) 
+main = print (((mySum 10.05) 2.1) 45.7)
+
+mySum :: Double -> (Double -> (Double -> Double))
+mySum a b c = a + b + c
