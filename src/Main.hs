68635808e04b4a9be418bfd060bcfb9f4f7354c1
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 03:44:23 +0300

    real carrying example

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,43 @@
--- import Data.Text
--- import Helpers
+type Login = String
+type Password = String
+type AvatarURL = String
+type UserId = Integer
 
-main = 
+userInfo :: Login -> Password -> AvatarURL -> UserId -> String
+userInfo login password avatarURL userId =
+  "Full info about user @" ++ (show userId) ++ ":" ++
+  "\n login: " ++ login ++
+  "\n password: " ++ password ++
+  "\n avatar URL: " ++ avatarURL
+
+type EmptyInfo = Login -> Password -> AvatarURL -> UserId -> String
+type WithLogin =          Password -> AvatarURL -> UserId -> String
+type AndWithPassword =                AvatarURL -> UserId -> String
+type AndWithAvatarURL =                            UserId -> String
+
+storeLoginIn :: EmptyInfo -> UserId -> WithLogin
+storeLoginIn emptyInfo userId =
+  emptyInfo "denis"
+  {- В реальности логин будет получен
+  в соответствии с переданным userId -}
+
+storePasswordIn :: WithLogin -> UserId -> AndWithPassword
+storePasswordIn infoWithLogin userId = infoWithLogin "123456789abc"
+  {- В реальности пароль будет получен
+  в соответствии с переданным userId -}
+
+storeAvatarURLIn :: AndWithPassword -> UserId -> AndWithAvatarURL
+storeAvatarURLIn infoWithPassword userId =
+  infoWithPassword "http://di-s.ru/denis_avatar.png"
+  {- В реальности URL будет получен
+  в соответствии с переданным userId -}
+
+main =
   let 
-    partsum = (mySum 10.05) 2.1
-  in 
-    print (partsum 55)
-
-mySum :: Double -> (Double -> (Double -> Double))
-mySum a b c = a + b + c
+    userId = 1234
+    infoWithLogin = storeLoginIn userInfo userId
+    infoWithPassword = storePasswordIn infoWithLogin userId
+    infoWithAvatarURL = storeAvatarURLIn infoWithPassword userId
+    fullInfoAboutUser = infoWithAvatarURL userId
+  in
+    putStrLn fullInfoAboutUser
