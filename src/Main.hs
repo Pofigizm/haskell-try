aa494af42d113ef01ec77118066e51ffae0348c6
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 14:30:31 +0300

    elem function - context of type

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-
+{-
 main = print $
   if "yellow" `elem` colors
   then "Yello is here!"
@@ -7,3 +7,16 @@ main = print $
                   "black",
                   "yellow",
                   "green"]
+-}
+
+-- elem :: Eq a => a -> [a] -> Bool
+-- Eq a => a
+--    (==) :: a -> a -> Bool
+--    (/=) :: a -> a -> Bool
+
+-- nothing :: a -> a
+-- nothing val = val
+-- main = print $ nothing $ IP "127.0.0.1"
+
+-- nothing :: (Show a, Show b, Eq b) => a -> b -> String
+-- nothing value1 value2 = show value1 ++ show value2
