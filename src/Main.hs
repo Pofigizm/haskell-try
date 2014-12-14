f4396a1991037897b32845f1b429268875a5750b
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 16:36:49 +0300

    standart IO()

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,5 @@
-data User = User { firstName
-                 , lastName
-                 , email :: String
-                 , account
-                 , uid :: Integer
-                 }
-
-changeEmail :: User -> String -> User
-changeEmail user newEmail = user { email = newEmail }
-
-main =
-  let userWithNewEmail = changeEmail user "shev.denis@gmail.com"
-  in print $ email userWithNewEmail
-  where user = User { firstName = "Denis"
-                    , lastName = "Shevchenko"
-                    , email = "me@dshevchenko.biz"
-                    , account = 24
-                    , uid = 15
-                    }
+main :: IO ()
+main = do
+  putStrLn "Input your text, please:"
+  lineFromUser <- getLine
+  putStrLn $ "Not bad: " ++ lineFromUser
