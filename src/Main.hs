c42b87e40c7de4a2e8ef73e97797347cbb5612e3
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 03:21:20 +0300

    carrying function with separate call

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 -- import Data.Text
 -- import Helpers
 
-main = print (((mySum 10.05) 2.1) 45.7)
+main = 
+  let 
+    partsum = (mySum 10.05) 2.1
+  in 
+    print (partsum 55)
 
 mySum :: Double -> (Double -> (Double -> Double))
 mySum a b c = a + b + c
