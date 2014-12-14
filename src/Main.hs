94b3aa9e2949140cae954275d69bee9a2693af7d
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 16:49:17 +0300

    function return -> send answer, not stop process

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
-obtainUserText :: String -> IO String
-obtainUserText prompt = do
-  putStrLn prompt -- Выведи приглашение ввести строку.
-  getLine -- Получи от пользователя некую строку.
+obtainTwoTextsFromUser :: IO String
+obtainTwoTextsFromUser = do
+  putStrLn "Enter your text, please: "
+  firstText <- getLine
+  putStrLn "One more, please: "
+  secondText <- getLine
+  return $ "'" ++ firstText ++ "' and '" ++ secondText ++ "'"
 
 main :: IO ()
 main = do
-  firstText <- obtainUserText "Enter your text, please: "
-  secondText <- obtainUserText "One more, please: "
-  putStrLn $ "You said '" ++ firstText ++ "' and '" ++ secondText ++ "'"
+  twoTexts <- obtainTwoTextsFromUser
+  putStrLn $ "You said " ++ twoTexts
