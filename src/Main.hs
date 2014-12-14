2490a0f30035127e5c914ff6f867152eca866b59
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 17:03:53 +0300

    throw exeption on top

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,4 @@
-obtainTwoTextsFromUser :: IO String
-obtainTwoTextsFromUser = do
-  putStrLn "Enter your text, please: "
-  firstText <- getLine
-  putStrLn "One more, please: "
-  secondText <- getLine
-  return $ "'" ++ firstText ++ "' and '" ++ secondText ++ "'"
-  putStrLn "Function return not stop run function :) "
-  getLine
-
 main :: IO ()
 main = do
-  twoTexts <- obtainTwoTextsFromUser
-  putStrLn $ "You said " ++ twoTexts
+  fileContent <- readFile "c://dev/test.c"
+  putStrLn fileContent
