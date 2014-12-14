1846eaad214f9d809538c66e21a85ae7c316e0c8
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 14:04:54 +0300

    many construction in one class

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
-data IPAddress = IP String
+data IPAddress = IP String | Host String
 
 instance Show IPAddress where
   show (IP address) =
+    address
+
+  show (Host address) =
     if address == "127.0.0.1" then "localhost" else address
 
-main = putStrLn $ show $ IP "127.0.0.1"
+
+main = putStrLn $ 
+  (show $ IP "127.0.0.1")
+  ++ " " ++ 
+  (show $ Host "127.0.0.1")
