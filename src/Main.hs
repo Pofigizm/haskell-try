34af819af4d71e4e17de4d99fad81d4ce48edb16
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 20:12:32 +0300

    throw own exception! ERRORgit add --allgit add --all! page 124 New Branch

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,25 @@
 import Control.Exception
+import Data.String.Utils
+import Data.Typeable
+
+type Repo = String
+
+data InvalidRepository = InvalidRepository Repo
+                         deriving (Show, Typeable)
+
+instance Exception InvalidRepository
+
+extractProtocol :: String -> String
+extractProtocol path =
+  if path `startsWith` "git" || path `startsWith` "ssh"
+  then takeWhile (/= ':') path
+  else throw $ InvalidRepository path
+  where startsWith url prefix = startswith prefix url
 
 main :: IO ()
 main = do
-  result <- try $ evaluate $ 2 `div` 0
-                  :: IO (Either SomeException Integer)
+  result <- try $ evaluate $ extractProtocol "ss://ul@sch/proj.git"
+                  :: IO (Either SomeException String)
   case result of
     Left exception -> putStrLn $ "Fault: " ++ show exception
-    Right value -> print value
+    Right protocol -> putStrLn protocol
