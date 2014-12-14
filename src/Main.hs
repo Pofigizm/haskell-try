cbafddb1c9297c036c627da6567f45886f5ad71b
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 10:36:11 +0300

    String is array of Char

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
-listOfNames :: String -> [String]
+listOfNames :: String -> [[Char]]
 listOfNames prefix =
   [prefix ++ "John", prefix ++ "Anna", prefix ++ "Andrew"]
-  
+
 main = print $ listOfNames "Dear "
