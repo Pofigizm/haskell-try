284a90c80fc39dbf8cfcfd21c3434ba7258d2df2
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 10:34:06 +0300

    simple array (list)

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,5 @@
-import Data.Char
-import Data.String.Utils
-
-addPrefix :: String -> String
-addPrefix url =
-  if url `startsWith` prefix then url else prefix ++ url
-  where
-    prefix = "http://"
-    startsWith url prefix = startswith prefix url
-
-encodeAllSpaces = replace " " "%20" -- Заменяем все пробелы на %20.
-
-makeItLowerCase = map toLower -- Переводим символы строки в нижний регистр.
-
-main =
-  putStrLn $ addPrefix . encodeAllSpaces . makeItLowerCase $ url
-  where url = "www.SITE.com/test me/Start page"
+listOfNames :: String -> [String]
+listOfNames prefix =
+  [prefix ++ "John", prefix ++ "Anna", prefix ++ "Andrew"]
+  
+main = print $ listOfNames "Dear "
