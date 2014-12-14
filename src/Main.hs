931d621679484f1b73faaac4e1609adf1389c905
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 11:22:19 +0300

    simple ranges of chars or digits

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,4 @@
-removeAllEmptyNamesFrom :: [String] -> [String]
-removeAllEmptyNamesFrom listOfNames =
-  filter notEmptyName listOfNames
-  where notEmptyName = not . null
 
-addSmth :: [String] -> [String]
-addSmth listOfNames =
-  map addDear listOfNames
-  where addDear el = "Dear " ++ el
+-- main = print ['a'..'z']
 
-main =
-  print $ addSmth $ removeAllEmptyNamesFrom listOfNames
-  where listOfNames = ["John", "", "Ann"]
+main = print [1..100]
