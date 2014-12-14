08fa4adce75a86bef1cdf3064d6faa39f89aead1
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 17:07:45 +0300

    catch throwing exception

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,13 @@
+import Control.Exception
+
+tryToOpenFile :: FilePath -> IO String
+tryToOpenFile path =
+  readFile path `catch` possibleErrors
+  where
+    possibleErrors :: IOException -> IO String
+    possibleErrors error = return $ show error
+
 main :: IO ()
 main = do
-  fileContent <- readFile "c://dev/test.c"
+  fileContent <- tryToOpenFile "c://dev/test.c"
   putStrLn fileContent
