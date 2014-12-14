cc0761f9733cc6adaa047943898f59cd499edd4b
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 13:58:21 +0300

    use sort names in previous commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
-data IPAddress = IPAddress String
+data IPAddress = IP String
 
 instance Show IPAddress where
-  show (IPAddress address) =
+  show (IP address) =
     if address == "127.0.0.1" then "localhost" else address
 
-main = putStrLn $ show $ IPAddress "127.0.0.1"
+main = putStrLn $ show $ IP "127.0.0.1"
