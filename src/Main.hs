8d32ac6835d5af5bcfbf36a42efdb20873307cbe
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 12:32:20 +0300

    more lists [OPERATION_with_ELEMs | ELEM1 <- LIST1, ..., ELEMN <- LISTN ]

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,9 @@
-import Data.Char
 
-main = print [toUpper c | c <- "http",  c /= 'h', c /= 'p']
--- "TT"
+main =
+  print [prefix ++ name | name <- names, prefix <- namePrefix]
+  where
+    names = ["James", "Victor", "Denis", "Michael"]
+    namePrefix = ["Mr. ", "Dear "]
+
+-- ["Mr. James","Dear James","Mr. Victor","Dear Victor",
+--  "Mr. Denis","Dear Denis","Mr. Michael","Dear Michael"]
