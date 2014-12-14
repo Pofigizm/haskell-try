abc79c4169798e3b6b25fd2bccd6f2f5eb61b189
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 11:00:32 +0300

    all data is immutable -> concat elem to aray return new array

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
+addNewHostToFront :: String -> [String] -> [String]
+addNewHostToFront newHost listOfHosts = newHost : listOfHosts
 
-main = 
-  print $ length listOfAnimals
-  where listOfAnimals = ["Bear", "Tiger", "Lion", "Wolf"]
+main =
+  print $ addNewHostToFront "124.67.54.90" listOfHosts
+  where
+    listOfHosts = ["45.67.78.89", "123.45.65.54", "127.0.0.1"]
