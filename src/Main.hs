b64bd14afb6a258b8507f3c8ff721da5d58ce6d8
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 12:14:35 +0300

    List comprehension / crafty list [OPERATION ELEM | ELEM <- LIST]

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,4 @@
+import Data.Char
 
-chessMove :: (String, String) -> String
-chessMove pair = fst pair ++ "-" ++ snd pair
-
-main = print $ chessMove ("e2", "e4")
+main = print [toUpper c | c <- "http"]
+-- "HTTP"
