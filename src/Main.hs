5ad885ee0c8c2d3c4adff9da9b99a3b37df06b3c
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 20:37:12 +0300

    Manad - IO - sequentially composition (>>=) :: IO a -> (a -> IO b) -> IO b

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,9 @@
-import Control.Exception
-import Data.String.Utils
-import Data.Typeable
-
-type Repo = String
-
-data InvalidRepository = InvalidRepository Repo
-                         deriving (Show, Typeable)
-
-instance Exception InvalidRepository
-
-extractProtocol :: String -> String
-extractProtocol path =
-  if path `startsWith` "git" || path `startsWith` "ssh"
-  then takeWhile (/= ':') path
-  else throw $ InvalidRepository path
-  where startsWith url prefix = startswith prefix url
-
+{-
 main :: IO ()
 main = do
-  result <- try $ evaluate $ extractProtocol "ss://ul@sch/proj.git"
-                  :: IO (Either SomeException String)
-  case result of
-    Left exception -> putStrLn $ "Fault: " ++ show exception
-    Right protocol -> putStrLn protocol
+  text <- getLine
+  putStrLn $ "You said '" ++ text ++ "'"
+-}
+
+main :: IO ()
+main = getLine >>= \text -> putStrLn $ "Your text is " ++ text
