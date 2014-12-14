3fd62d4433b159a32cf97fef176cf5959daf127e
Denis Iogansen <pofigizm@gmail.com>
2014-12-14 14:40:20 +0300

    component type looks like object with strict types

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,13 @@
-{-
-main = print $
-  if "yellow" `elem` colors
-  then "Yello is here!"
-  else "There's no yellow..."
-  where colors = ["red",
-                  "black",
-                  "yellow",
-                  "green"]
--}
+data User = User { firstName :: String
+                 , lastName :: String
+                 , email :: String
+                 }
 
--- elem :: Eq a => a -> [a] -> Bool
--- Eq a => a
---    (==) :: a -> a -> Bool
---    (/=) :: a -> a -> Bool
-
--- nothing :: a -> a
--- nothing val = val
--- main = print $ nothing $ IP "127.0.0.1"
-
--- nothing :: (Show a, Show b, Eq b) => a -> b -> String
--- nothing value1 value2 = show value1 ++ show value2
+main =
+  print $ firstName user ++ " " ++
+  lastName user ++ ", " ++
+  email user
+  where user = User { firstName = "Denis"
+                    , lastName = "Shevchenko"
+                    , email = "me@dshevchenko.biz"
+                    }
